3b009b9d8bc689121a4c4d1bb9337d7fb7a44089
Azka Siddiqui <azkasiddiqui240@gmail.com>
2023-10-01 19:19:48 -0400

    first commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Red Herring.
+Red Herring is a proof of concept (PoC) organization for profitable detection of fake news.
---
+++ pages/app.js
@@ -0,0 +1,37 @@
+import { ChakraProvider } from '@chakra-ui/react'
+import { Global, css } from "@emotion/react";
+
+const GlobalStyle = ({ children }) => {
+  return (
+    <>
+      <Global
+        styles={css`
+          html {
+            min-width: 356px;
+            scroll-behavior: smooth;
+          }
+          #__next {
+            display: flex;
+            flex-direction: column;
+            min-height: 100vh;
+            background: #fffff;
+          }
+        `}
+      />
+      {children}
+    </>
+  );
+};
+
+
+function MyApp({ Component, pageProps }) {
+  return (
+    <ChakraProvider>
+      <GlobalStyle>
+        <Component {...pageProps} />
+      </GlobalStyle>
+    </ChakraProvider>
+  )
+}
+
+export default MyApp
